3de32f653f047b8e1079b18a1d73e87347aa2b06
Abd Allah Zidan <eng.abdallahzidan@gmail.com>
2019-12-17 00:08:39 +0200

    Additn new functions [adding node to the list and writing it in the file ] , [reading the file  at the start of the program and adding the nodes ] , [search for a book using name ] , [getting the size of the list]

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
+/*
+ * A console program to manage a library of books 
+ * you can add new books providing the book details as  book id , book name , book author and  book price
+ * you can search for a book using name or id 
+ * you can edit book's details 
+ * you can remove a book using it's id
+ * books are saved in a dat file
+ * */
+ 
 struct Book{
         int id;
         char name[40];
@@ -23,24 +32,48 @@ void printBook(struct Book book);
 
 // list functions //
 struct node * createnode(struct Book book);
-int addnode(struct Book book);
+int addnode(struct node** phead,struct node** ptail, char path[] , struct Book book);
+int readFileIntoList(struct node* * phead , struct node** ptail,char path[]);
+void printList(struct node** phead);
+int getSize(struct node** phead);
+struct Book * search(struct node* phead,char name[]);
 int removeBook(int id);
-
-
 
 
 
 int main(int argc, char **argv)
 {
-	
+
+    char path[] = "books.dat";
+    char val[] = "Alchemist";
+    struct node* phead = NULL;
+    struct node* ptail = NULL;
+
+    readFileIntoList(&phead,&ptail,path);
     struct Book book = fillBookData();
-    printBook(book);
+    addnode(&phead ,&ptail,path,book ) ;
+    printList(&phead);
+    
+    struct Book* retval = search(phead,val);
+    if(retval){
+        printf("found ..... ");
+        printBook(*retval);
+    }
+    
+    
+    printf("\n\nsize : %d\n",getSize(&phead));
     
 	return 0;
 }
 
 
 // book functions 
+
+/*
+ * fill bookData  
+ * return book struct
+ * */
+ 
 struct Book  fillBookData(){
    struct Book book;
     printf("Enter the book id: ");
@@ -55,13 +88,162 @@ struct Book  fillBookData(){
     return book;
 }
 
+
+/*
+ * print book details 
+ * return void
+ * */
+ 
 void printBook(struct Book book){
     printf("\n");
-    // printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf("book's id :   %d \n",book.id);
     printf("book's name:   %s \n",book.name);
     printf("book's author:  %s \n",book.author);
     printf("book's price:   %d  \n",book.price);
-  //  printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf("\n");
+}
+
+
+
+
+// list functions
+
+
+// createnode function return a pointer to node 
+
+struct node * createnode( struct Book book){
+            struct node * ptr = (struct node *) malloc(sizeof(struct node));
+            if(ptr){
+                ptr->book = book;
+                ptr->next = NULL;
+                ptr->prev = NULL;
+            }
+            
+            return ptr;
+}
+
+// addnode function ... append a node to the list and reecord the entry to the file 
+
+int addnode(struct node** phead,struct node** ptail, char path[] , struct Book book){
+    FILE * outfile ;
+    int retval =0;
+    struct node* ptr = createnode(book);
+    if(ptr){
+        if(!*phead){
+            *phead =*ptail = ptr;
+        }
+        else{
+            ptr->prev = *ptail;
+            (*ptail)->next = ptr;
+            *ptail = ptr;
+        }
+        
+        outfile = fopen ("books.dat", "a"); 
+        
+        // check if the file is opened 
+        if (outfile == NULL) 
+            { 
+                fprintf(stderr, "\nError opening the file .. data may not be saved \n"); 
+       
+            } 
+        else{
+                        // writing the content of book struct into the file 
+        
+                        fwrite (&(ptr->book),sizeof(struct Book),1,outfile);
+        
+                        printf("book added and contents to file written successfully !\n"); 
+                        fclose (outfile);
+        }
+        
+        
+     
+        retval =1;
+    }
+    
+    return retval;
+}
+
+
+int readFileIntoList(struct node** phead , struct node** ptail, char path[]  ){
+    int retval =0;
+    struct Book input;
+    FILE * infile ;
+    infile = fopen("books.dat", "r");
+     if(infile != NULL){
+        while(fread(&input, sizeof(struct Book), 1, infile)) {
+            // add each struct into the list
+               struct node* ptr = createnode(input);
+                if(ptr){
+                if(!*phead){
+                    *phead =*ptail = ptr;
+                }
+            else{
+                ptr->prev = *ptail;
+                (*ptail)->next = ptr;
+                *ptail = ptr;
+            }
+        }
+      
+   
+    }
+     // close file 
+    fclose (infile); 
+    retval =1;
+     }
+    return retval;
+
+}
+
+
+
+// print the whole list of books
+
+void printList(struct node** phead){
+    struct node * temp = *phead;
+    if(!temp){
+        printf("\nLibrary is empty.\n");
+    }
+    else{
+        printf("\n[[\n");
+        
+           while(temp){
+                printBook(temp->book);
+                temp= temp->next;
+                printf("\n***********************************************\n");
+            }
+            
+            printf("\n]]\n");
+    }
+ 
+}
+
+
+// get list size
+int getSize(struct node** phead){
+    int size =0;
+    struct node* temp = *phead;
+    while(temp){
+        size ++;
+        temp = temp->next;
+    }
+    return size;
+    
+}
+
+// search function
+
+struct Book * search(struct node* phead,char name[]){
+    struct node* temp = phead;
+    struct Book* ptr = NULL;
+    while (temp){
+        if(strcmp(temp->book.name,name) == 0){
+            ptr =& (temp->book);
+            temp = NULL;
+        }
+        else{
+            temp = temp->next;
+        }
+    }
+    
+    return ptr;
 }
